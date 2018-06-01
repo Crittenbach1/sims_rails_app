3fd653c8a79420d92d0edb3ae6cbbea58cdc16e3
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-01 16:03:22 -0400

    creates rooms controller

---
+++ app/controllers/rooms_controller.rb
@@ -0,0 +1,33 @@
+class RoomsController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def delete
+
+  end
+
+  private
+    def room_params
+
+    end
+
+end
