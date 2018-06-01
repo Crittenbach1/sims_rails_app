6ab5325ba675a98486beb02edfc9b13bf2cbfeff
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-01 15:52:06 -0400

    added houses root path and creates a houses controller

---
+++ app/controllers/houses_controller.rb
@@ -0,0 +1,25 @@
+class HousesController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  private
+    def house_params
+
+    end
+
+end
