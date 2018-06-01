5a2c5458b66c954d1cee1cfa65e8324f70ffd3d9
Cynthia Rittenbach <crittenbach@gmail.com>
2018-06-01 16:05:12 -0400

    added a furniture controller

---
+++ app/controllers/furniture_controller.rb
@@ -0,0 +1,33 @@
+class FurnitureController < ApplicationController
+  before_action :authenticate_user!
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def delete
+
+  end
+
+  private
+    def furniture_params
+
+    end
+
+end
